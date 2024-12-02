84419515405f7f73ee57f7565a518ff6253a0d18
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-12-02 19:27:18 +0600

    m-56 complete

--- index.js
+++ index.js
@@ -1,11 +1,93 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j0hxo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+	const mangoCollection = client.db('mangoDB').collection('mango');
+
+	app.get('/mango', async(req, res) =>{
+		const cursor = mangoCollection.find();
+		const result = await cursor.toArray();
+		res.send(result);
+	})
+
+	app.get('/mango/:id', async(req, res) =>{
+		const id = req.params.id;
+		const query = {_id: new ObjectId(id)};
+		const result = await mangoCollection.findOne(query);
+		res.send(result);
+	})
+
+	app.post('/mango', async(req, res) =>{
+		const newCoffee = req.body;
+		console.log(newCoffee);
+		const result = await mangoCollection.insertOne(newCoffee);
+		res.send(result);
+	})
+	
+	app.put('/mango/:id', async(req, res) =>{
+		const id = req.params.id;
+		const filter = {_id: new ObjectId(id)};
+		const options = {upsert: true};
+		const updatedCoffee = req.body;
+
+		const coffee = {
+			$set: {
+				name: updatedCoffee.name, 
+				chef: updatedCoffee.chef, 
+				supplier: updatedCoffee.supplier, 
+				taste: updatedCoffee.taste, 
+				category: updatedCoffee.category, 
+				details: updatedCoffee.details, 
+				photo: updatedCoffee.photo
+			}
+		}
+		const result = await mangoCollection.updateOne(filter, coffee, options);
+		res.send(result);
+	})
+
+	app.delete('/mango/:id', async(req, res)=>{
+		const id = req.params.id;
+		const query = {_id: new ObjectId(id)};
+		const result = await mangoCollection.deleteOne(query);
+		res.send(result);
+	})
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) =>{
