7872e3a9b0cd8e8a53f57b0faa935ba77ed6d694
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-12-03 00:49:57 +0600

    commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j0hxo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri);
+
 
 
 
@@ -30,6 +30,9 @@ async function run() {
     await client.connect();
 
 	const mangoCollection = client.db('mangoDB').collection('mango');
+
+	//users collection data client
+	const userCollection = client.db('mangoDB').collection('users');
 
 	app.get('/mango', async(req, res) =>{
 		const cursor = mangoCollection.find();
@@ -78,6 +81,41 @@ async function run() {
 		const result = await mangoCollection.deleteOne(query);
 		res.send(result);
 	})
+
+	//users related apis 
+
+	app.get('/users', async(req, res) =>{
+		const cursor = userCollection.find();
+		const result = await cursor.toArray();
+		res.send(result);
+	})
+	app.post('/users/', async(req, res)=>{
+		const newUser = req.body;
+		console.log(newUser);
+		const result = await userCollection.insertOne(newUser);
+		res.send(result);
+	});
+
+	app.patch('/users', async(req, res)=>{
+		const email = req.body.email;
+		const filter = {email};
+		const updatedDoc = {
+			$set:{
+				lastSignInTime: req.body?.lastSignInTime
+			}
+		}
+		const result = await userCollection.updateOne(filter, updatedDoc);
+		res.send(result);
+	})
+
+
+	app.delete('/users/:id', async(req, res)=>{
+		const id = req.params.id;
+		const query = {_id: new ObjectId(id)};
+		const result = await userCollection.deleteOne(query);
+		res.send(result);
+	})
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
